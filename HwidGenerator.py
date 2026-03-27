ab2a413ac375b502c5e6227ec33e50f0568a9aee
nyzrak <seloe@me.com>
2026-03-27 03:08:08 +0100

    Initial commit. Added HwidGenerator.

---
+++ HwidGenerator.py
@@ -0,0 +1,14 @@
+import hashlib
+import wmi
+
+
+class HWIDGenerator:
+    @staticmethod
+    def get_hwid() -> str:
+        wmi_data = wmi.WMI()
+        cpu_id = wmi_data.Win32_Processor()[0].ProcessorId.strip().encode('utf-8')
+        drive_serial = wmi_data.Win32_LogicalDisk(DeviceID='C:')[0].VolumeSerialNumber.strip().encode('utf-8')
+        disk_serial = wmi_data.Win32_PhysicalMedia()[0].SerialNumber.strip().encode('utf-8')
+
+        return hashlib.sha256(cpu_id + drive_serial + disk_serial).hexdigest()
+
