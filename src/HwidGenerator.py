0569dbef15a7c78f0683dc9d523ef92447140c58
nyzrak <seloe@me.com>
2026-03-30 21:55:48 +0200

    add-functionality-for-all-platforms: add logic to differentiate between os

--- src/HwidGenerator.py
+++ src/HwidGenerator.py
@@ -5,6 +5,18 @@ from wmi import WMI
 class HWIDGenerator:
     @staticmethod
     def get_hwid() -> str:
+        os = platform.system()
+        if 'Windows' == os:
+            return HWIDGenerator._get_windows_hwid()
+        elif 'Darwin' == os:
+            return HWIDGenerator._get_mac_hwid()
+        elif 'Linux' == os:
+            return HWIDGenerator._get_linux_hwid()
+        else:
+            raise NotImplementedError(f'Unsupported OS: {os}')
+
+    @staticmethod
+    def _get_windows_hwid() -> str:
         wmi_data = WMI()
         cpu_id = wmi_data.Win32_Processor()[0].ProcessorId.strip().encode("utf-8")
         drive_serial = (
@@ -17,3 +29,11 @@ class HWIDGenerator:
         )
 
         return sha256(cpu_id + drive_serial + disk_serial).hexdigest()
+
+    @staticmethod
+    def _get_mac_hwid() -> str:
+        command = 'sysctl -n machdep.cpu.brand_string'
+
+    @staticmethod
+    def _get_linux_hwid() -> str:
+        pass
