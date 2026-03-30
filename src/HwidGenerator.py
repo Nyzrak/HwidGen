d771985d965fa28c5f9950bcd9c2218073d64a45
nyzrak <seloe@me.com>
2026-03-30 20:01:30 +0200

    ran ruff

--- src/HwidGenerator.py
+++ src/HwidGenerator.py
@@ -1,14 +1,19 @@
-import hashlib
-import wmi
+from hashlib import sha256
+from wmi import WMI
+
 
 class HWIDGenerator:
     @staticmethod
     def get_hwid() -> str:
-        wmi_data = wmi.WMI()
-        cpu_id = wmi_data.Win32_Processor()[0].ProcessorId.strip().encode('utf-8')
-        drive_serial = wmi_data.Win32_LogicalDisk(DeviceID='C:')[0].VolumeSerialNumber.strip().encode('utf-8')
-        disk_serial = wmi_data.Win32_PhysicalMedia()[0].SerialNumber.strip().encode('utf-8')
+        wmi_data = WMI()
+        cpu_id = wmi_data.Win32_Processor()[0].ProcessorId.strip().encode("utf-8")
+        drive_serial = (
+            wmi_data.Win32_LogicalDisk(DeviceID="C:")[0]
+            .VolumeSerialNumber.strip()
+            .encode("utf-8")
+        )
+        disk_serial = (
+            wmi_data.Win32_PhysicalMedia()[0].SerialNumber.strip().encode("utf-8")
+        )
 
-        return hashlib.sha256(cpu_id + drive_serial + disk_serial).hexdigest()
-
-print(HWIDGenerator.get_hwid())
+        return sha256(cpu_id + drive_serial + disk_serial).hexdigest()
